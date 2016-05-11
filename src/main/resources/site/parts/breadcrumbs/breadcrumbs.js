15a935aeb08b3aa15f43f6abea56fbceb433f9d3
Tor Løkken <tor.lokken@gmail.com>
2016-05-11 14:46:30 +0200

    Breadcrumb fix

--- src/main/resources/site/parts/breadcrumbs/breadcrumbs.js
+++ src/main/resources/site/parts/breadcrumbs/breadcrumbs.js
@@ -1,30 +1,27 @@
-var UTIL = require('/lib/enonic/util/util');
-var portal = require('/lib/xp/portal');
-var thymeleaf = require('/lib/xp/thymeleaf');
+var libs = {
+    portal: require('/lib/xp/portal'),
+    thymeleaf: require('/lib/xp/thymeleaf'),
+    content: require('/lib/xp/content'),
+    util: require('/lib/enonic/util'),
+    menu: require('/lib/enonic/menu')
+};
 
 // Handle GET request
 exports.get = handleGet;
 
 function handleGet(req) {
-    var me = this;
-
-    function renderView() {
-        var view = resolve('breadcrumbs.html');
-        var model = createModel();
-
-        return {
-            body: thymeleaf.render(view, model)
-        };
-    }
+    var view = resolve('breadcrumbs.html');
+    var model = createModel();
 
     function createModel() {
         var model = {};
 
-        var site = portal.getSite();
-        var content = portal.getContent();
+        model.breadcrumbTrail = getBreadcrumbTrail();
 
-        UTIL.log(site);
-        UTIL.log(content);
+        //libs.util.log(breadcrumbTrail);
+
+        //libs.util.log(site);
+        //libs.util.log(content);
 
 
         return model;
@@ -32,35 +29,52 @@ function handleGet(req) {
 
 
 
-    /*function getBreadcrumbTrail() {
-        var content = execute('portal.getContent');
-        var fullPath = content._path;
+    function getBreadcrumbTrail() {
+        var content = libs.portal.getContent();
 
+        libs.util.log(content);
+
+        // Remove leading slash
+        var fullPath = content._path.replace(/^\/|\/$/g, '');
         var pathArray = fullPath.split('/');
 
+        //libs.util.log(pathArray);
 
         var breadCrumbTrail = [];
 
+        for (var i = 1; i <= pathArray.length; i++) {
+
+            var currentPath = '/' + pathArray.slice(0, i).join('/');
+
+            libs.util.log(currentPath);
+
+            breadCrumbTrail.push(getBreadcrumb(currentPath))
+        }
+
+        //libs.util.log(breadCrumbTrail);
 
 
-
+        return breadCrumbTrail;
 
 
     }
 
     function getBreadcrumb(path) {
         var breadcrumb = {};
-        var result = execute('content.get', {
+        var result = libs.content.get({
             key: path
         });
 
         if (result) {
-            breadcrumb.path =
+            //breadcrumb.path = null;
+            //libs.util.log(result);
         }
 
-        return breadcrumb;
+        return result;
 
-    }*/
+    }
 
-    return renderView();
+    return {
+        body: libs.thymeleaf.render(view, model)
+    };
 }
