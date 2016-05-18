cd1defe044ea16e06a0694f997988221d1fa085b
Tor Løkken <tor.lokken@gmail.com>
2016-05-18 10:27:42 +0200

    Initial work on sample video part

---
+++ src/main/resources/site/parts/sample-video/sample-video.js
@@ -0,0 +1,25 @@
+var libs = {
+    portal: require('/lib/xp/portal'),
+    thymeleaf: require('/lib/xp/thymeleaf'),
+    util: require('/lib/enonic/util')
+};
+
+// Handle GET request
+exports.get = handleGet;
+
+function handleGet(req) {
+    var component = libs.portal.getComponent();
+    var view = resolve('sample-video.html');
+    var model = createModel();
+
+    function createModel() {
+        var model = {};
+        model.aspectRatio = component.config.aspectRatio ? component.config.aspectRatio : 'widescreen';
+        model.label = component.config.label;
+        return model;
+    }
+
+    return {
+        body: libs.thymeleaf.render(view, model)
+    };
+}
