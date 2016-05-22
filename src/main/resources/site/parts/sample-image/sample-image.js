8720f130d799c35b9cac948ca91633590419fcd9
Tor Løkken <tor.lokken@gmail.com>
2016-05-22 06:57:25 +0200

    Added sample image

---
+++ src/main/resources/site/parts/sample-image/sample-image.js
@@ -0,0 +1,40 @@
+var libs = {
+    portal: require('/lib/xp/portal'),
+    thymeleaf: require('/lib/xp/thymeleaf'),
+    util: require('/lib/enonic/util')
+};
+
+// Handle GET request
+exports.get = handleGet;
+
+function handleGet(req) {
+    var component = libs.portal.getComponent();
+    var view = resolve('sample-image.html');
+    var model = createModel();
+
+    function createModel() {
+        var model = {};
+        model.aspectRatio = component.config.aspectRatio ? component.config.aspectRatio : 'widescreen';
+        model.label = component.config.label;
+        model.caption = component.config.caption;
+
+
+        model.linkUrl = getLinkUrl();
+
+        return model;
+    }
+
+    function getLinkUrl() {
+        var linkUrl;
+        if (component.config['linkedPage']) {
+            linkUrl = libs.portal.pageUrl({
+                id: component.config['linkedPage']
+            });
+        }
+        return linkUrl;
+    }
+
+    return {
+        body: libs.thymeleaf.render(view, model)
+    };
+}
