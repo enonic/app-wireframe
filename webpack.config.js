dba1f5a524209129b7c38e1a3328ef65b38e404b
Alan Semenov <ase@enonic.com>
2020-08-06 10:40:57 +0200

    Upgraded NPM dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
     output: {
         filename: 'main.min.js'
     },
     optimization: {
-        minimizer: [new UglifyJsPlugin()]
+        minimizer: [
+            new TerserPlugin({
+                sourceMap: !isProd,
+                terserOptions: {
+                    compress: {
+                        drop_console: false
+                    },
+                    keep_classnames: true,
+                    keep_fnames: true
+                }
+            })
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
@@ -15,5 +28,5 @@ module.exports = {
             "window.jQuery": "jquery"
         })
     ],
-    mode: 'production'
+    mode: isProd ? 'production' : 'development'
 };
