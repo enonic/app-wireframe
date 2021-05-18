8298b11a9c0fe2db574ffa8e8420d561c5ee631e
Persijn Kwekkeboom <pkw@enonic.com>
2021-05-18 15:00:44 +0200

    fix: webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,142 @@
-const TerserPlugin = require('terser-webpack-plugin');
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
+const globImporter = require("node-sass-glob-importer");
 
-const isProd = process.env.NODE_ENV === 'production';
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlMinifierPlugin = require("html-minifier-webpack-plugin");
 
-const SRC_DIR = "/src/main/resources/assets/";
-const DST_DIR = "/build/resources/main/assets";
-const outputPath = path.join(__dirname, DST_DIR);
+const isProd = true;
 
-module.exports = {
+const SRC_DIR = "src/main/resources";
+const DST_DIR = "build/resources/main";
+
+const ABS_SRC_DIR = path.resolve(__dirname, SRC_DIR);
+const ABS_DST_DIR = path.resolve(__dirname, DST_DIR);
+
+JS_CONFIG = {
+    context: ABS_SRC_DIR,
     entry: {
-        "js/main.min.js" : SRC_DIR + "js/main.js"
+        main: "./assets/js/main.js",
     },
     output: {
-        path: outputPath,
-        filename: './[name]'
+        path: ABS_DST_DIR,
+        filename: "assets/js/[name].min.js",
     },
     optimization: {
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    compress: {
-                        drop_console: false
-                    },
-                    keep_classnames: true,
-                    keep_fnames: true
-                }
-            })
-        ]
+        minimizer: [new UglifyJsPlugin()],
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
-            "window.jQuery": "jquery"
-        })
+            "window.jQuery": "jquery",
+        }),
     ],
-    mode: isProd ? 'production' : 'development'
+    mode: isProd ? "production" : "development",
 };
+
+STYLE_ASSETS_CONFIG = {
+    stats: {
+        colors: true,
+        hash: false,
+        modules: false,
+        version: false,
+        moduleTrace: false,
+    },
+    context: ABS_SRC_DIR,
+    entry: {
+        main: "./assets/css/main.scss",
+    },
+    output: {
+        path: ABS_DST_DIR,
+        filename: "css/[name].bundle.js", // Empty js bundle
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: { publicPath: "../" },
+                    },
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [["postcss-preset-env", {}]],
+                            },
+                        },
+                    },
+                    {
+                        loader: "sass-loader", // compiles Sass to CSS
+                        options: {
+                            sourceMap: isProd,
+                            sassOptions: {
+                                //errLogToConsole: true,
+                                importer: globImporter(),
+                                outputStyle: "compressed",
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf)(\?.*$|$)/,
+                type: "asset/inline",
+            },
+            {
+                test: /\.(jpe|jpg|png|svg)(\?.*$|$)/,
+                use: {
+                    // May conflict with image minification?
+                    loader: "url-loader",
+                    options: {
+                        limit: 100000,
+                        name: "./assets/img/[name].[ext]",
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "./assets/css/[name].css",
+        }),
+        new ImageminPlugin({
+            disable: !isProd, // Disable during development
+            pngquant: {
+                quality: "95-100",
+            },
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            test: /\.(jpe?g|png|gif|svg)$/i,
+        }),
+        new HtmlMinifierPlugin({
+            collapseWhitespace: true,
+            removeComments: true,
+            keepClosingSlash: true,
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: "**/*.html",
+                    to: "[path][name][ext]",
+                },
+                {
+                    from: "assets/img/**/*.svg",
+                    to: "[path][name][ext]",
+                },
+            ],
+        }),
+    ],
+    performance: {
+        hints: false // Unsure if this is a good thing to ignore
+    },
+    mode: isProd ? "production" : "development",
+};
+
+module.exports = [STYLE_ASSETS_CONFIG, JS_CONFIG];
